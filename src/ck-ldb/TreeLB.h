b64dcef7a3996169d660e951a6fa26f1aeee0d64
PathikritGhosh <pathikrit216@gmail.com>
2022-03-01 21:26:43 -0600

    Add path

--- src/ck-ldb/TreeLB.h
+++ src/ck-ldb/TreeLB.h
@@ -46,7 +46,7 @@ class LevelLogic
         std::vector <std::string> features{"f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11",
                                            "f12", "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f20", "f21", "f22",
                                            "f23", "f24"};
-        xgboost = fastforest::load_txt("XGBoost/model/model.txt", features, 7);
+        xgboost = fastforest::load_txt("src/ck-ldb/XGBoost/model/model.txt", features, 7);
       }
     }
 
