af2945a1194ab22fbb2e7b6ab821cbb6b615caeb
PathikritGhosh <pathikrit216@gmail.com>
2022-03-05 22:40:25 -0600

    Remove DEBAD

--- src/ck-ldb/MetaBalancer.C
+++ src/ck-ldb/MetaBalancer.C
@@ -303,9 +303,9 @@ bool MetaBalancer::AddLoad(int it_n, double load) {
   int index = it_n % VEC_SIZE;
   total_count_vec[index]++;
   adaptive_struct.total_syncs_called++;
-  DEBAD("At PE %d Total contribution for iteration %d is %d \
+  DEBAD(("At PE %d Total contribution for iteration %d is %d \
       total objs %d\n", CkMyPe(), it_n, total_count_vec[index],
-      lbmanager->GetObjDataSz());
+      lbmanager->GetObjDataSz()));
 
   if (it_n <= adaptive_struct.finished_iteration_no) {
     CkAbort("Error!! Received load for iteration that has contributed\n");
